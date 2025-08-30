f4275ff6af772568f43c1206644f12c11317a5e2
daksh0103-prog <chananadaksh14@gmail.com>
2025-08-30 08:55:26 +0530

    Add files via upload

---
+++ loan_prediction (2).py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# -----------------------------
+# Load trained model
+# -----------------------------
+@st.cache_resource
+def load_model():
+    model = joblib.load("loan_pred.pkl")   # apna model file ka naam dalna
+    return model
+
+model = load_model()
+
+# -----------------------------
+st.image('https://images.pexels.com/photos/4386321/pexels-photo-4386321.jpeg')
+# Title
+st.title(" Loan Eligibility Prediction Using Machine Learning")
+
+st.write("""
+Loan Prediction using Machine Learning is an essential problem in the banks and finance industries.  
+Accurately predicting whether a loan will be approved or rejected can help financial institutions manage risk, reduce defaults, and increase profitability.
+""")
+
+# Sidebar input form
+st.sidebar.header("Enter Applicant Details")
+st.sidebar.image('https://media1.tenor.com/m/v8zmj3VSaPMAAAAC/loan-need-a-loan.gif')
+
+gender = st.sidebar.selectbox("Gender", ("Male", "Female"))
+married = st.sidebar.selectbox("Married", ("Yes", "No"))
+dependents = st.sidebar.selectbox("Dependents", ("0", "1", "2", "3+"))
+education = st.sidebar.selectbox("Education", ("Graduate", "Not Graduate"))
+self_employed = st.sidebar.selectbox("Self Employed", ("Yes", "No"))
+applicant_income = st.sidebar.number_input("Applicant Income", min_value=0, step=100)
+coapplicant_income = st.sidebar.number_input("Coapplicant Income", min_value=0, step=100)
+loan_amount = st.sidebar.number_input("Loan Amount", min_value=0, step=10)
+loan_amount_term = st.sidebar.number_input("Loan Amount Term (in days)", min_value=0, step=10)
+credit_history = st.sidebar.selectbox("Credit History", (1.0, 0.0))
+property_area = st.sidebar.selectbox("Property Area", ("Urban", "Semiurban", "Rural"))
+
+# Collect input into dataframe
+input_data = {
+    "Gender": [gender],
+    "Married": [married],
+    "Dependents": [dependents],
+    "Education": [education],
+    "Self_Employed": [self_employed],
+    "ApplicantIncome": [applicant_income],
+    "CoapplicantIncome": [coapplicant_income],
+    "LoanAmount": [loan_amount],
+    "Loan_Amount_Term": [loan_amount_term],
+    "Credit_History": [credit_history],
+    "Property_Area": [property_area]
+}
+
+df = pd.DataFrame(input_data)
+
+# Show final input
+st.subheader(" Final Input Data")
+st.write(df)
+
+# Prediction
+if st.button("Predict Loan Status"):
+    prediction = model.predict(df)[0]
+    if prediction == 1:
+        st.success(" Loan will be APPROVED!")
+    else:
+        st.error(" Loan will be REJECTED!")
+
+# Show algorithms used (static text)
+st.markdown("""
+---
+### Algorithms Used:
+Logistic Regression
+Naive Bayes
+Support Vector Machine (Linear)
+K-Nearest Neighbors
+Decision Tree
+Random Forest
+XGBoost
+Artificial Neural Network (1 Hidden Layer, Keras)
+""")
